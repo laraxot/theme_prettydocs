9b2b0771708c3c78b04985342017bd66c3b30166
marco76tv <marco.sottana@gmail.com>
2020-10-15 15:54:17 +0200

    .

---
+++ Main_files/zlotte/readme.md
@@ -0,0 +1,3 @@
+# Pretty Docs
+
+This project was made with Laravel framework for documents handling. You can upload document, make him private or public, manage them by signing in, voting for them etc. 
---
+++ Resources/views/layouts/partials/htmlheader.blade.php
@@ -0,0 +1,20 @@
+<head>
+    <title>Bootstrap 4 project documentation theme for developers</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <!-- FontAwesome JS -->
+    <script defer src="{{ asset('/plugins/fontawesome/js/all.js') }}"></script>
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="{{ asset('/plugins/bootstrap/css/bootstrap.min.css') }}">
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="{{ asset('/plugins/elegant_font/css/style.css') }}">
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="{{ Theme::asset('pub_theme::css/styles.css') }}">
+
+</head>
